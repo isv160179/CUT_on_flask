621cefdf873b302325f47ab063ebfdef308d15e9
Сергей Иванов <isv1601@yandex.ru>
2024-01-08 14:32:49 +0300

    Проект подготовлен

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+SHORT_URL_LENGHT = 16
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'KEY')
--- yacut/models.py
+++ yacut/models.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 
+from settings import SHORT_URL_LENGHT
 from yacut import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column(db.String, nullable=False)
+    short = db.Column(db.String(SHORT_URL_LENGHT), unique=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
